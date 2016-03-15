095e35b16d3ad9a2763e189371b13f1baae1d91c
linsenss <linsen_ss@sina.com>
2016-03-15 09:07:09 +0800

    更新代码

--- www/app/app.js
+++ www/app/app.js
@@ -31,39 +31,39 @@ angular
         $ionicPlatform.ready(function () {
             if (ionic.Platform.isAndroid()) {
 
-                cordova.getAppVersion.getVersionCode(function (versionCode) {
-                    var newVersionCode = 28;
-                    if (versionCode < newVersionCode) {
-                        ConfirmModalDialogService.AsyncConfirmYesNo("版本有更新，是否需要升级？",
-                            function () {
-                                var url = "http://download.canguanwuyou.cn/download/cgwy_car.apk";
-                                var targetPath = cordova.file.externalApplicationStorageDirectory + 'cgwy/cgwy_car_' + newVersionCode + '.apk';
-                                var trustHosts = true;
-                                var options = {};
-                                $cordovaFileTransfer.download(url, targetPath, options, trustHosts)
-                                    .then(function (result) {
-                                        // 打开下载下来的APP
-                                        $cordovaFileOpener2.open(targetPath, 'application/vnd.android.package-archive')
-                                            .then(function () {
-                                            }, function (err) {
-                                                ConfirmModalDialogService.AsyncAlert("文件打开失败，请稍后重试！");
-                                            });
-                                    }, function (err) {
-                                        ConfirmModalDialogService.AsyncAlert("当前网络不稳定,下载失败!");
-                                    }, function (progress) {
-                                        $timeout(function () {
-                                            var downloadProgress = (progress.loaded / progress.total) * 100;
-                                            var msg = "已经下载:" + Math.floor(downloadProgress) + "%";
-                                            ConfirmModalDialogService.AsyncDialogShow("下载进度" , msg);
-                                            if (downloadProgress >= 99) {
-                                                ConfirmModalDialogService.AsyncDialogHide();
-                                            }
-                                        })
-                                    });
-                            }
-                        );
-                    } else {
-
+                //cordova.getAppVersion.getVersionCode(function (versionCode) {
+                //    var newVersionCode = 28;
+                //    if (versionCode < newVersionCode) {
+                //        ConfirmModalDialogService.AsyncConfirmYesNo("版本有更新，是否需要升级？",
+                //            function () {
+                //                var url = "http://download.canguanwuyou.cn/download/cgwy_car.apk";
+                //                var targetPath = cordova.file.externalApplicationStorageDirectory + 'cgwy/cgwy_car_' + newVersionCode + '.apk';
+                //                var trustHosts = true;
+                //                var options = {};
+                //                $cordovaFileTransfer.download(url, targetPath, options, trustHosts)
+                //                    .then(function (result) {
+                //                        // 打开下载下来的APP
+                //                        $cordovaFileOpener2.open(targetPath, 'application/vnd.android.package-archive')
+                //                            .then(function () {
+                //                            }, function (err) {
+                //                                ConfirmModalDialogService.AsyncAlert("文件打开失败，请稍后重试！");
+                //                            });
+                //                    }, function (err) {
+                //                        ConfirmModalDialogService.AsyncAlert("当前网络不稳定,下载失败!");
+                //                    }, function (progress) {
+                //                        $timeout(function () {
+                //                            var downloadProgress = (progress.loaded / progress.total) * 100;
+                //                            var msg = "已经下载:" + Math.floor(downloadProgress) + "%";
+                //                            ConfirmModalDialogService.AsyncDialogShow("下载进度" , msg);
+                //                            if (downloadProgress >= 99) {
+                //                                ConfirmModalDialogService.AsyncDialogHide();
+                //                            }
+                //                        })
+                //                    });
+                //            }
+                //        );
+                //    } else {
+                //
                         if (NetworkUtil.getNetworkRs()) {
                             var updateObject = function () {
                                 UpdateService.updateApp().then(function (result) {
@@ -77,8 +77,8 @@ angular
                             }
                             updateObject();
                         }
-                    }
-                });
+                //    }
+                //});
 
             }
         });
