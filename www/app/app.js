a5b76e4c04e56a0ef772919a03f935ef46051ae9
linsenss <linsen_ss@sina.com>
2016-07-26 13:48:03 +0800

    增加双击返回退出

--- www/app/app.js
+++ www/app/app.js
@@ -27,44 +27,44 @@ angular
         //"host": "",
         //"environment": "develop"
     })
-    .run(function ($ionicPlatform, $cordovaFile,$cordovaFileOpener2, $cordovaFileTransfer,$timeout,ConfirmModalDialogService,$state,UpdateService,NetworkUtil) {
+    .run(function ($ionicPlatform, $cordovaFile,$cordovaFileOpener2,$ionicHistory,$rootScope,$cordovaToast, $cordovaFileTransfer,$timeout,ConfirmModalDialogService,$state,UpdateService,NetworkUtil) {
 
         $ionicPlatform.ready(function () {
             if (ionic.Platform.isAndroid()) {
 
-                //cordova.getAppVersion.getVersionCode(function (versionCode) {
-                //    var newVersionCode = 28;
-                //    if (versionCode < newVersionCode) {
-                //        ConfirmModalDialogService.AsyncConfirmYesNo("版本有更新，是否需要升级？",
-                //            function () {
-                //                var url = "http://download.canguanwuyou.cn/download/cgwy_car.apk";
-                //                var targetPath = cordova.file.externalApplicationStorageDirectory + 'cgwy/cgwy_car_' + newVersionCode + '.apk';
-                //                var trustHosts = true;
-                //                var options = {};
-                //                $cordovaFileTransfer.download(url, targetPath, options, trustHosts)
-                //                    .then(function (result) {
-                //                        // 打开下载下来的APP
-                //                        $cordovaFileOpener2.open(targetPath, 'application/vnd.android.package-archive')
-                //                            .then(function () {
-                //                            }, function (err) {
-                //                                ConfirmModalDialogService.AsyncAlert("文件打开失败，请稍后重试！");
-                //                            });
-                //                    }, function (err) {
-                //                        ConfirmModalDialogService.AsyncAlert("当前网络不稳定,下载失败!");
-                //                    }, function (progress) {
-                //                        $timeout(function () {
-                //                            var downloadProgress = (progress.loaded / progress.total) * 100;
-                //                            var msg = "已经下载:" + Math.floor(downloadProgress) + "%";
-                //                            ConfirmModalDialogService.AsyncDialogShow("下载进度" , msg);
-                //                            if (downloadProgress >= 99) {
-                //                                ConfirmModalDialogService.AsyncDialogHide();
-                //                            }
-                //                        })
-                //                    });
-                //            }
-                //        );
-                //    } else {
-                //
+                cordova.getAppVersion.getVersionCode(function (versionCode) {
+                    var newVersionCode = 38;
+                    if (versionCode < newVersionCode) {
+                        ConfirmModalDialogService.AsyncConfirmYesNo("版本有更新，是否需要升级？",
+                            function () {
+                                var url = "http://download.canguanwuyou.cn/download/cgwy_car.apk";
+                                var targetPath = cordova.file.externalApplicationStorageDirectory + 'cgwy/cgwy_car_' + newVersionCode + '.apk';
+                                var trustHosts = true;
+                                var options = {};
+                                $cordovaFileTransfer.download(url, targetPath, options, trustHosts)
+                                    .then(function (result) {
+                                        // 打开下载下来的APP
+                                        $cordovaFileOpener2.open(targetPath, 'application/vnd.android.package-archive')
+                                            .then(function () {
+                                            }, function (err) {
+                                                ConfirmModalDialogService.AsyncAlert("文件打开失败，请稍后重试！");
+                                            });
+                                    }, function (err) {
+                                        ConfirmModalDialogService.AsyncAlert("当前网络不稳定,下载失败!");
+                                    }, function (progress) {
+                                        $timeout(function () {
+                                            var downloadProgress = (progress.loaded / progress.total) * 100;
+                                            var msg = "已经下载:" + Math.floor(downloadProgress) + "%";
+                                            ConfirmModalDialogService.AsyncDialogShow("下载进度" , msg);
+                                            if (downloadProgress >= 99) {
+                                                ConfirmModalDialogService.AsyncDialogHide();
+                                            }
+                                        })
+                                    });
+                            }
+                        );
+                    } else {
+
                         if (NetworkUtil.getNetworkRs()) {
                             var updateObject = function () {
                                 UpdateService.updateApp().then(function (result) {
@@ -78,8 +78,28 @@ angular
                             }
                             updateObject();
                         }
-                //    }
-                //});
+                    }
+                });
+
+
+                $ionicPlatform.registerBackButtonAction(function (e) {
+                    //alert($ionicHistory.currentStateName());
+                    if ($rootScope.backButtonPressedOnceToExit) {
+                        ionic.Platform.exitApp();
+                    } else if (!$rootScope.devMode &&
+                        ($ionicHistory.currentStateName().indexOf("home") == 0 || $ionicHistory.currentStateName().indexOf("login") == 0)) {
+
+                        $rootScope.backButtonPressedOnceToExit = true;
+                        $cordovaToast.show("再按一次返回退出", 'short', 'bottom');
+                        setTimeout(function () {
+                            $rootScope.backButtonPressedOnceToExit = false;
+                        }, 2000);
+                    }else if ($ionicHistory.backView()) {
+                        $ionicHistory.goBack();
+                    }
+                    e.preventDefault();
+                    return false;
+                }, 101);
 
             }
         });
